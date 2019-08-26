f9a86782ccd949c4fcaf62ed72bb1605ac1eb832
Gaurav <vgsinha@gmail.com>
2019-08-25 21:04:23 -0700

    one liner

--- facebook/kclosest.go
+++ facebook/kclosest.go
@@ -1,29 +1,9 @@
 import "sort"
 
-type info struct {
-	distance int
-	point    []int
-}
-
 func kClosest(points [][]int, K int) [][]int {
-	result := make([]info, 0)
-
-	for _, point := range points {
-		result = append(result, info{distance: findDistance(point), point: point})
-	}
-
-	sort.Slice(result, func(a, b int) bool {
-		return result[a].distance < result[b].distance
+	sort.Slice(points, func(a, b int) bool {
+		return (points[a][0]*points[a][0] + points[a][1]*points[a][1]) < (points[b][0]*points[b][0] + points[b][1]*points[b][1])
 	})
 
-	res := make([][]int, 0)
-
-	for _, r := range result {
-		res = append(res, r.point)
-	}
-	return res[:K]
-}
-
-func findDistance(point []int) int {
-	return point[0]*point[0] + point[1]*point[1]
+	return points[:K]
 }
