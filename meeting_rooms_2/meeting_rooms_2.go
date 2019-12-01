9bc8b1b732fac712820fcb46aeb76fae07bd1e36
Gaurav Sinha <vgsinha@gmail.com>
2019-12-01 12:58:11 -0800

    Update meeting_rooms_2.go

--- meeting_rooms_2/meeting_rooms_2.go
+++ meeting_rooms_2/meeting_rooms_2.go
@@ -1,45 +1,37 @@
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
 
-Example 1:
-
+/*
 Input: [[0, 30],[5, 10],[15, 20]]
 Output: 2
-Example 2:
 
-Input: [[7,10],[2,4]]
-Output: 1
+rooms: [30, 10, ]
+sort the input by start time. 
+create a room when 
+    - there is no room with end time less than equal to my start time. 
+        - add the room by append to array and add my end time there. 
+    - else if found a room - then update the room availability to end time of this meeting. 
 
+*/ 
 
-import "sort"
-
-func minMeetingRooms(intervals []Interval) int {
-    rooms := make([]int, 0) 
-    if len(intervals) == 0 {
-        return 0
-    }
-    
-    sort.Slice(intervals, func(a, b int) bool {
-        return intervals[a].Start < intervals[b].Start
+func minMeetingRooms(intervals [][]int) int {
+    rooms := make([]int, 0)
+    sort.Slice(intervals, func (a,b int)bool {
+        return intervals[a][0] < intervals[b][0]
     })
     
-    rooms = append(rooms, 0) // first room available from time = 0
-    //rooms store available_at for each room 
+
     for _, meeting := range intervals {
-        found := false 
-        for room_number, room := range rooms {
-            if meeting.Start >= room {
-                //meeting start >= room availability
-                
-                //update availability to end of this meeting for this room
-                rooms[room_number] = meeting.End
-                found = true
-                break
-            } 
-        }
-        if found == false {
-           //no room found - create one, available after this meeting 
-            rooms = append(rooms, meeting.End)
-        }
-    } 
+            found := false 
+            for index, room := range rooms {
+                if room <= meeting[0] {
+                    rooms[index] = meeting[1]
+                    found = true 
+                    break
+                }      
+            }
+            if !found {
+                rooms = append(rooms, meeting[1])
+            }
+    }
+    
     return len(rooms)
 }
